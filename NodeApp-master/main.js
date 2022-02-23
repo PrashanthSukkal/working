849b7a2339c306c8ac7695eb0e73583b9aea39a5
Prashanth Sukkal <94683076+PrashanthSukkal@users.noreply.github.com>
2022-02-23 15:28:56 +0530

    Update main.js

--- NodeApp-master/main.js
+++ NodeApp-master/main.js
@@ -7,6 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Hello NodeApp!! \n");
 });
 
-// listen on localhost:80
-server.listen(80);
-console.log("Server listening on port 80 :  http://127.0.0.1:80/");
+// listen on localhost:8080
+server.listen(8080);
+console.log("Server listening on port 8080 :  http://localhost:8080/");
